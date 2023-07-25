6fde36f2472916b6e375315d2411c493fa01801a
chisoo <cltn222@naver.com>
2023-07-25 19:07:36 +0900

    A+B

---
+++ src/greedy/APlusB.java
@@ -0,0 +1,13 @@
+package greedy;
+import java.util.*;
+
+public class APlusB {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int A,B;
+        A=scan.nextInt();
+        B=scan.nextInt();
+        System.out.println(A+B);
+
+    }
+}
